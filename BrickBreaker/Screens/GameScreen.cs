e478f4e0e84fc14f6ce1546912b4bc274a51c064
Cjeran <cierdieb649@ed.amdsb.ca>
2019-05-09 09:49:39 -0400

    Powerup distance before deletion

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -268,7 +268,7 @@ namespace BrickBreaker
                     break;
                 }
                     //delete power up if it goes off the screen
-                    if (p.y > paddle.y + 30)
+                    if (p.y > paddle.y + 50)
                     {
                         powers.Remove(powers[0]);
                         break;
